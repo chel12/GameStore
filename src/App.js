af424902370ef318e7edd7367bc5f10bf3915c7a
chel12 <sema.chel12@gmail.com>
2024-02-20 15:31:27 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './scss/app.scss';
+
+import { Header } from './components/Header/Header';
+import { Categories } from './components/Categories/Categories';
+import { Sort } from './components/Sort/Sort';
+import { GameBlock } from './components/GameBlock/GameBlock';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="wrapper">
+			<Header />
+			<div className="content">
+				<div className="container">
+					<div className="content__top">
+						<Categories />
+						<Sort />
+					</div>
+					<h2 className="content__title">Все игры</h2>
+					<div className="content__items">
+						<GameBlock title={'GTA VI'} price={4990} />
+						<GameBlock title={'GTA V'} price={3990} />
+						<GameBlock title={'GTA IV'} price={2990} />
+						<GameBlock title={'GTA III'} price={1990} />
+						<GameBlock title={'GTA II'} price={990} />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Categories = () => {
+	return (
+		<div className="categories">
+			<ul>
+				<li className="active">Все</li>
+				<li>Action</li>
+				<li>Racing</li>
+				<li>Strategy</li>
+				<li>Sports</li>
+				<li>Flight simulation</li>
+			</ul>
+		</div>
+	);
+};
