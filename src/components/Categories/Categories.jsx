563c643235a9f9cff6f588fe608e83c66efbdfb4
chel12 <sema.chel12@gmail.com>
2024-02-20 16:31:52 +0300

    Categories:Logic

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Categories = () => {
+	const [activeIndex, setActiveIndex] = useState(0);
+
+	// const onClickCategory = (index) => {  вариант для сложных
+	// 	setActiveIndex(index);
+	//   ...
+	//   ...
+	// };
+
+	//setActiveIndex(index) лайт варик дял простого
+
+	const categories = [
+		'Все',
+		'Action',
+		'Racing',
+		'Strategy',
+		'Sports',
+		'Flight simulation',
+	];
+
 	return (
 		<div className="categories">
 			<ul>
-				<li className="active">Все</li>
+				{categories.map((li, index) => (
+					<li
+						onClick={() => setActiveIndex(index)}
+						className={activeIndex == index ? 'active' : ''}>
+						{li}
+					</li>
+				))}
+				{/* <li className="active">Все</li>
 				<li>Action</li>
 				<li>Racing</li>
 				<li>Strategy</li>
 				<li>Sports</li>
-				<li>Flight simulation</li>
+				<li>Flight simulation</li> */}
 			</ul>
 		</div>
 	);
