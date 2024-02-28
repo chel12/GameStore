5d3f5992ee6cadf0989020a681c276ddde32810c
Semyon <sema.chel12@gmail.com>
2024-02-28 09:09:24 +0300

    роутер добавлен, страница 404

--- src/App.js
+++ src/App.js
@@ -1,61 +1,19 @@
 import './scss/app.scss';
 
 import { Header } from './components/Header/Header';
-import { Categories } from './components/Categories/Categories';
-import { Sort } from './components/Sort/Sort';
-import { GameBlock } from './components/GameBlock/GameBlock';
-import { useEffect, useState } from 'react';
-import GameBlockSkeleton from './components/GameBlock/GameBlockSkeleton';
+import Home from './pages/Home';
+import { NotFound } from './pages/NotFound';
 
 function App() {
 	//https://e7feb94fe973f168.mokky.dev/items
-	const [items, setItems] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
-
-	useEffect(() => {
-		fetch('https://e7feb94fe973f168.mokky.dev/items')
-			.then((res) => {
-				//в джейсон
-				return res.json();
-			})
-			.then((arr) => {
-				setItems(arr);
-				setIsLoading(false);
-			}); //используем джейсон
-	}, []);
 
 	return (
 		<div className="wrapper">
 			<Header />
 			<div className="content">
 				<div className="container">
-					<div className="content__top">
-						<Categories />
-						<Sort />
-					</div>
-					<h2 className="content__title">Все игры</h2>
-					<div className="content__items">
-						{isLoading
-							? [...new Array(8)].map((_, index) => (
-									<GameBlockSkeleton key={index} />
-							  ))
-							: items.map((item) =>
-									isLoading ? (
-										<GameBlockSkeleton />
-									) : (
-										<GameBlock
-											title={item.title}
-											price={item.price}
-											id={item.id}
-											imgUrl={item.imgUrl}
-											types={item.types}
-											editions={item.editions}
-											category={item.category}
-											rating={item.rating}
-										/>
-									)
-							  )}
-					</div>
+					{/* <Home /> */}
+					<NotFound />
 				</div>
 			</div>
 		</div>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+
+import { Categories } from '../components/Categories/Categories';
+import { Sort } from '../components/Sort/Sort';
+import { GameBlock } from '../components/GameBlock/GameBlock';
+import GameBlockSkeleton from '../components/GameBlock/GameBlockSkeleton';
+
+const Home = () => {
+	const [items, setItems] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		fetch('https://e7feb94fe973f168.mokky.dev/items')
+			.then((res) => {
+				//в джейсон
+				return res.json();
+			})
+			.then((arr) => {
+				setItems(arr);
+				setIsLoading(false);
+			}); //используем джейсон
+	}, []);
+	return (
+		<>
+			<div className="content__top">
+				<Categories />
+				<Sort />
+			</div>
+			<h2 className="content__title">Все игры</h2>
+			<div className="content__items">
+				{isLoading
+					? [...new Array(8)].map((_, index) => (
+							<GameBlockSkeleton key={index} />
+					  ))
+					: items.map((item) =>
+							isLoading ? (
+								<GameBlockSkeleton />
+							) : (
+								<GameBlock
+									title={item.title}
+									price={item.price}
+									id={item.id}
+									imgUrl={item.imgUrl}
+									types={item.types}
+									editions={item.editions}
+									category={item.category}
+									rating={item.rating}
+								/>
+							)
+					  )}
+			</div>
+		</>
+	);
+};
+export default Home;
