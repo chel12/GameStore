ed0410adf871f8c7facc49f9b5f0fba11c8175a7
Semyon <sema.chel12@gmail.com>
2024-02-28 17:31:44 +0300

    notfound доделать

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import './scss/app.scss';
-
+import { Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header/Header';
 import Home from './pages/Home';
-import { NotFound } from './pages/NotFound';
+import NotFound from './pages/NotFound';
+
+import './scss/app.scss';
+import { Cart } from './pages/Cart';
 
 function App() {
 	//https://e7feb94fe973f168.mokky.dev/items
@@ -12,8 +14,11 @@ function App() {
 			<Header />
 			<div className="content">
 				<div className="container">
-					{/* <Home /> */}
-					<NotFound />
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/cart" element={<Cart />} />
+						<Route path="*" element={<NotFound />} />
+					</Routes>
 				</div>
 			</div>
 		</div>
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { NotFoundBlock } from '../components/NotFoundBlock/NotFoundBlock';
 
-export const NotFound = () => {
-	return <h1>Такой страницы не найдено :( </h1>;
+const NotFound = () => {
+	return <NotFoundBlock />;
 };
+
+export default NotFound;
