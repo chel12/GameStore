722cffe008a161050a8f1643bf7a49c1af073f5a
Semyon <sema.chel12@gmail.com>
2024-03-06 13:50:40 +0300

    добавление addEventListener для клика вне pop

--- src/components/Sort/Sort.jsx
+++ src/components/Sort/Sort.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setSort } from '../../redux/slices/filterSlice';
 
@@ -30,6 +30,8 @@ export const sortList = [
 ];
 
 export const Sort = () => {
+	//ссылка на dom элемент чтобы попап скрывать на клик в другом месте
+	const sortRef = useRef();
 	const dispatch = useDispatch();
 	const sort = useSelector((state) => state.filter.sort);
 
@@ -39,9 +41,21 @@ export const Sort = () => {
 		dispatch(setSort(i));
 		setOpen(!open);
 	};
+	useEffect(() => {
+		const handleClickOutside = (event) => {
+			if (!event.composedPath().includes(sortRef.current)) {
+				setOpen(false);
+			}
+		};
+		document.body.addEventListener('click', handleClickOutside);
+
+		return () => {
+			document.body.removeEventListener('click', handleClickOutside);
+		};
+	}, []);
 
 	return (
-		<div className="sort">
+		<div ref={sortRef} className="sort">
 			<div className="sort__label">
 				<svg
 					width="10"
